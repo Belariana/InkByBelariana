9e5fe6b92bedd8426413a6d1d6d14f26d4c2c0dc
Arina <belariania@gmail.com>
2025-12-02 13:13:42 +0300

    Update forWork.js

--- js/forWork.js
+++ js/forWork.js
@@ -59,8 +59,8 @@ document.addEventListener('DOMContentLoaded', () => {
   // 2. ЗАГРУЗКА ВСЕХ НЕОБХОДИМЫХ БЛОКОВ
   // ===================================================================
 
-  loadComponent('header-placeholder', '/_header.html');
-  loadComponent('footer-placeholder', '/_footer.html');
+  loadComponent('header-placeholder', '/header.html');
+  loadComponent('footer-placeholder', '/footer.html');
   // Добавьте сюда другие блоки, если они появятся, например:
   // loadComponent('reviews-placeholder', './_reviews.html');
 
@@ -95,7 +95,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
   // Если на странице есть заглушка для подвала
   if (ctaPlaceholder) {
-    fetch('/_cta.html') // Загружаем содержимое файла _cta.html
+    fetch('/cta.html') // Загружаем содержимое файла _cta.html
       .then(response => {
         if (!response.ok) throw new Error('Network response was not ok');
         return response.text(); // Преобразуем ответ в текст
@@ -113,4 +113,5 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 }); // <-- Конец обработчика DOMContentLoaded
+
 
